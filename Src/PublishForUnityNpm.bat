e48f2121cd593639963b0bc04e6130a0862b7be3
Ankur Ranpariya <ranpariya.ankur@nukeboxstudios.com>
2022-10-19 00:39:35 +0530

    publlic batch file updated for path of the package.

--- Src/PublishForUnityNpm.bat
+++ Src/PublishForUnityNpm.bat
@@ -1,2 +1,3 @@
-dotnet publish com.iPAHeartBeat.Core.Abstraction.csproj -c Release --no-dependencies --framework net48 --output ../Packages/com.iPAHeartBeat.Core.Abstraction/Runtime/.
-del ..\Packages\com.iPAHeartBeat.Core.Abstraction\Runtime\Newtonsoft.Json.dll
+dotnet publish com.iPAHeartBeat.Core.Abstraction.csproj -c Release --no-dependencies --framework net48 --output ../Unity/Packages/com.iPAHeartBeat.Core.Abstraction/Runtime/.
+Rem next line commented as does project does not uses NewtonSoft.Json
+Rem del ../Unity\Packages\com.iPAHeartBeat.Core.Abstraction\Runtime\Newtonsoft.Json.dll

e0b4a8c17324ba55cec0362cef409c21818ac634
Ankur Ranpariya <ranpariya.ankur@nukeboxstudios.com>
2022-10-18 21:41:20 +0530

    Support File Added

---
+++ Src/PublishForUnityNpm.bat
@@ -0,0 +1,2 @@
+dotnet publish com.iPAHeartBeat.Core.Abstraction.csproj -c Release --no-dependencies --framework net48 --output ../Packages/com.iPAHeartBeat.Core.Abstraction/Runtime/.
+del ..\Packages\com.iPAHeartBeat.Core.Abstraction\Runtime\Newtonsoft.Json.dll
